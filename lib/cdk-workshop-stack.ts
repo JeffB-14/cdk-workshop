924d0df541e68101047105eb948b12381e8cad25
Jeff Beier <jeff.beier@cygrp.com>
2020-02-07 12:12:51 -0600

    removed sqs and added lambda

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,8 +1,14 @@
 import cdk = require('@aws-cdk/core');
+import lambda = require('@aws-cdk/aws-lambda');
 
 export class CdkWorkshopStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-    // nothing here!
+    // defines an AWS Lambda resource
+    const hello = new lambda.Function(this, 'HelloHandler', {
+      runtime: lambda.Runtime.NODEJS_12_X,      // execution environment
+      code: lambda.Code.asset('lambda'),  // code loaded from the "lambda" directory
+      handler: 'hello.handler'                // file is "hello", function is "handler"
+    });
   }
 }
